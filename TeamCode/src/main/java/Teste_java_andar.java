73ef1d177c66241efe425fd1ba60f60d86062559
Acro-Arthur <arthurend54@gmail.com>
2025-09-12 23:59:58 -0300

    fazendo

--- TeamCode/src/main/java/Teste_java_andar.java
+++ TeamCode/src/main/java/Teste_java_andar.java
@@ -69,46 +69,26 @@ public class Teste_java_andar extends LinearOpMode {
         //motor_direito_frente = hardwareMap.get(DcMotor.class, "direito_frente");
         motor_direito_tras = hardwareMap.get(DcMotor.class, "direito");
 
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         motor_esquerdo_tras.setDirection(DcMotor.Direction.REVERSE);
         motor_direito_tras.setDirection(DcMotor.Direction.FORWARD);
 
 
-        // Wait for the game to start (driver presses START)
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
 
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
             motor_esquerdo_tras.setPower(leftPower);
             motor_direito_tras.setPower(rightPower);
 
-            // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
